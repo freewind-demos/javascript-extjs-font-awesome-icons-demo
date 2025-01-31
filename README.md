8160e92eec14e6141ce4fb89edb68d59c1773a0a
Freewind <nowindlee@gmail.com>
2025-01-31 23:24:41 +0800

    javascript-extjs-font-awesome-icons-demo

--- README.md
+++ README.md
@@ -1,13 +1,51 @@
-JavaScript ExtJS CDN Demo
-=========================
+# ExtJS Font Awesome Icons Demo
 
-使用CDN上提供的extjs来写程序。找不到这方面的例子，一点点试出来要使用哪些文件（有几千个文件）。
+这个demo展示了如何在ExtJS应用中使用Font Awesome图标。通过CDN加载ExtJS和Font Awesome资源,实现了一个图标展示和使用示例。
+
+## 功能特点
+
+- 展示常用Font Awesome图标及其使用方法
+- 包含图标预览、名称、CSS类名和使用示例
+- 实际演示了在ExtJS工具栏中使用图标的效果
+
+## 技术栈
+
+- ExtJS 6.2.0 (通过CDN加载)
+- Font Awesome 4.7.0 (通过CDN加载)
+
+## 运行方法
+
+直接在浏览器中打开`index.html`即可查看demo效果。
+
+## 关键代码
+
+### 1. 引入必要的CSS和JS文件
+
+```html
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/extjs/6.2.0/classic/theme-classic/resources/theme-classic-all-debug.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/extjs/6.2.0/packages/font-awesome/resources/font-awesome-all.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/extjs/6.2.0/ext-all-debug.js"></script>
+```
+
+### 2. 在ExtJS组件中使用图标
+
+```javascript
+{
+    text: 'Add',
+    iconCls: 'fa fa-plus'
+}
+```
+
+## 相关资源
+
+- ExtJS CDN: https://cdnjs.com/libraries/extjs
+- Font Awesome CDN: https://cdnjs.com/libraries/font-awesome
+- Font Awesome 图标列表: https://fontawesome.com/v4/icons/
 
 ```
 open index.html
 ```
-
-![demo](./images/demo.jpg)
 
 Resources
 ---------
--- index.js
+++ index.js
@@ -1,6 +1,70 @@
 Ext.application({
-    name: 'Demo',
+    name: 'IconsDemo',
     launch: function () {
-        Ext.Msg.alert('Hello', 'ExtJS')
+        // 定义图标数据
+        var iconsData = [
+            { name: 'Add', cls: 'fa fa-plus', usage: "iconCls: 'fa fa-plus'" },
+            { name: 'Edit', cls: 'fa fa-edit', usage: "iconCls: 'fa fa-edit'" },
+            { name: 'Delete', cls: 'fa fa-trash', usage: "iconCls: 'fa fa-trash'" },
+            { name: 'Save', cls: 'fa fa-save', usage: "iconCls: 'fa fa-save'" },
+            { name: 'Search', cls: 'fa fa-search', usage: "iconCls: 'fa fa-search'" },
+            { name: 'Refresh', cls: 'fa fa-refresh', usage: "iconCls: 'fa fa-refresh'" },
+            { name: 'Settings', cls: 'fa fa-cog', usage: "iconCls: 'fa fa-cog'" },
+            { name: 'User', cls: 'fa fa-user', usage: "iconCls: 'fa fa-user'" },
+            { name: 'Home', cls: 'fa fa-home', usage: "iconCls: 'fa fa-home'" },
+            { name: 'Calendar', cls: 'fa fa-calendar', usage: "iconCls: 'fa fa-calendar'" }
+        ];
+
+        // 创建数据存储
+        var store = Ext.create('Ext.data.Store', {
+            fields: ['name', 'cls', 'usage'],
+            data: iconsData
+        });
+
+        // 创建网格面板
+        Ext.create('Ext.grid.Panel', {
+            title: 'ExtJS Common Icons Demo',
+            store: store,
+            columns: [{
+                text: 'Icon',
+                dataIndex: 'cls',
+                width: 100,
+                renderer: function (value) {
+                    return '<i class="' + value + '" style="font-size: 16px;"></i>';
+                }
+            }, {
+                text: 'Name',
+                dataIndex: 'name',
+                width: 100
+            }, {
+                text: 'CSS Class',
+                dataIndex: 'cls',
+                flex: 1
+            }, {
+                text: 'Usage Example',
+                dataIndex: 'usage',
+                flex: 1
+            }],
+            width: 800,
+            height: 400,
+            renderTo: 'main',
+            frame: true,
+            tbar: [{
+                text: 'Add',
+                iconCls: 'fa fa-plus'
+            }, {
+                text: 'Edit',
+                iconCls: 'fa fa-edit'
+            }, {
+                text: 'Delete',
+                iconCls: 'fa fa-trash'
+            }, {
+                text: 'Save',
+                iconCls: 'fa fa-save'
+            }, {
+                text: 'Refresh',
+                iconCls: 'fa fa-refresh'
+            }]
+        });
     }
-})
+});
